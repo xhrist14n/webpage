fb56910889442d3b601ac19d227e93fb6768bcad
khrist14n <khrist14n@gmail.com>
2016-08-23 00:32:21 -0500

    project

---
+++ git.sh
@@ -0,0 +1,15 @@
+clear
+git add ./test
+git add ./app
+git add bower.json
+git add generate_all.sh
+git add git.sh
+git add Gruntfile.js
+git add package.json
+git add .bowerrc
+git add  .editorconfig
+git add  .gitattributes
+git add  .gitignore
+git add .yo-rc.json
+git commit -am "project"
+git push origin master
